b12d5f4a157537d1de5e55e6a2408ca6701afcb4
Leonardo Collado Torres <lcolladotor@gmail.com>
2020-09-21 20:59:55 -0400

    Add megadepth_cmd() based on blogdown::hugo_cmd()

---
+++ R/megadepth.R
@@ -0,0 +1,22 @@
+#' Run Megadepth commands
+#'
+#' Wrapper functions to run Megadepth commands via
+#' \code{\link{system2}('megadepth', ...)}.
+#'
+#' @param ... Arguments to be passed to \code{system2('megadepth', ...)}, e.g.
+#' \code{annotation(path)} is basically
+#' \code{megadepth_cmd(c('--annotation', path))}
+#' (i.e. run the command \command{megadepth --annotation path}).
+#' @export
+#' @describeIn megadepth_cmd Run an arbitrary Megadepth command.
+#'
+#' @references
+#' This function is based on blogdown::hugo_cmd() which is available from
+#' <https://github.com/rstudio/blogdown/blob/master/R/hugo.R>.
+#'
+#' @examples
+#'
+#' megadepth_cmd("--version")
+megadepth_cmd <- function(...) {
+    system2(find_megadepth(), ...)
+}
