3063ccfa756bc3120591d9c48533ac7090874dec
Leonardo Collado Torres <lcolladotor@gmail.com>
2020-09-30 10:22:18 -0400

    Workaround the megadepth bam processing error on Windows by including the bam processing files in the package, and copying them to the location used by the rest of the bigwig tests
    ```
    C:\Users\fellg>C:\\Users\\fellg\\AppData\\Roaming\\Megadepth\\megadepth.exe D:/Dropbox/Code/megadepth/inst/tests/test.bam --prefix C:\\Users\\fellg\\AppData\\Local\\Temp\\RtmpKefDQ8/test.bam --threads 1 --no-head --bigwig --auc --min-unique-qual 10 --annotation  D:/Dropbox/Code/megadepth/inst/tests/test_exons.bed --frag-dist --alts --include-softclip --only-polya --read-ends --test-polya --no-annotation-stdout
    building whole annotation region map done
    2 chromosomes for annotated regions read
    Processing BAM: "D:/Dropbox/Code/megadepth/inst/tests/test.bam"
    [bwGetOverlappingIntervalsCore] Got an error
    [bwClose] There was an error while finishing writing a bigWig file! The output is likely truncated.
    [bwGetOverlappingIntervalsCore] Got an error
    [bwClose] There was an error while finishing writing a bigWig file! The output is likely truncated.
    Read 96 records
    ```

--- tests/testthat/test-megadepth_main_tests.R
+++ tests/testthat/test-megadepth_main_tests.R
@@ -24,14 +24,14 @@ if (FALSE) {
         mode = "wb"
     )
     download.file(
-      "https://github.com/ChristopherWilks/megadepth/raw/master/tests/test3.bam",
-      destfile = here::here("inst", "tests", "test3.bam"),
-      mode = "wb"
+        "https://github.com/ChristopherWilks/megadepth/raw/master/tests/test3.bam",
+        destfile = here::here("inst", "tests", "test3.bam"),
+        mode = "wb"
     )
     download.file(
-      "https://github.com/ChristopherWilks/megadepth/raw/master/tests/long_reads.bam",
-      destfile = here::here("inst", "tests", "long_reads.bam"),
-      mode = "wb"
+        "https://github.com/ChristopherWilks/megadepth/raw/master/tests/long_reads.bam",
+        destfile = here::here("inst", "tests", "long_reads.bam"),
+        mode = "wb"
     )
 }
 
@@ -39,38 +39,103 @@ if (FALSE) {
 install_megadepth()
 
 ## Create test files
-megadepth_shell(
-    pkg_file("tests", "test.bam"),
-    "prefix" = file.path(tempdir(), "test.bam"),
-    "threads" = 1,
-    "no-head" = TRUE,
-    "bigwig" = TRUE,
-    "auc" = TRUE,
-    "min-unique-qual" = 10,
-    "annotation" = pkg_file("tests", "test_exons.bed"),
-    "frag-dist" = TRUE,
-    "alts" = TRUE,
-    "include-softclip" = TRUE,
-    "only-polya" = TRUE,
-    "read-ends" = TRUE,
-    "test-polya" = TRUE,
-    "no-annotation-stdout" = TRUE
-)
+if (!xfun::is_windows()) {
+    ## This currently fails on Windows
+    megadepth_shell(
+        pkg_file("tests", "test.bam"),
+        "prefix" = file.path(tempdir(), "test.bam"),
+        "threads" = 1,
+        "no-head" = TRUE,
+        "bigwig" = TRUE,
+        "auc" = TRUE,
+        "min-unique-qual" = 10,
+        "annotation" = pkg_file("tests", "test_exons.bed"),
+        "frag-dist" = TRUE,
+        "alts" = TRUE,
+        "include-softclip" = TRUE,
+        "only-polya" = TRUE,
+        "read-ends" = TRUE,
+        "test-polya" = TRUE,
+        "no-annotation-stdout" = TRUE
+    )
 
-## Copy test bigwig file to the package dir
-if (FALSE) {
-    file.copy(file.path(tempdir(), "test.bam.all.bw"), here::here("inst", "tests", "test.bam.all.bw"), overwrite = TRUE)
-}
+    if (FALSE) {
+        ## Copy test files to the package for the other tests on Windows
+        test_files <- dir(tempdir(), "test.bam", full.names = TRUE)
+        dir.create(here::here("inst", "tests", "test_output_files"),
+            showWarnings = FALSE)
+        sapply(test_files,
+            file.copy,
+            here::here("inst", "tests", "test_output_files/"))
+    }
+} else {
+    ## Copy the test files
+    test_files <-
+        dir(pkg_file("tests", "test_output_files"),
+            "test.bam",
+            full.names = TRUE)
+    sapply(test_files, file.copy, tempdir())
 
-## Run AUC test
+    ## For trying to debug on Windows
+    if (FALSE) {
+        manual_cmd <- paste(
+            pkg_file("tests", "test.bam"),
+            "--prefix",
+            file.path(tempdir(), "test.bam"),
+            "--threads 1",
+            "--no-head",
+            "--bigwig",
+            "--auc",
+            "--min-unique-qual 10",
+            "--annotation ",
+            pkg_file("tests", "test_exons.bed"),
+            "--frag-dist",
+            "--alts",
+            "--include-softclip",
+            "--only-polya",
+            "--read-ends",
+            "--test-polya",
+            "--no-annotation-stdout"
+        )
+        megadepth_cmd(manual_cmd)
+        paste(find_megadepth(), manual_cmd)
+        # C:\\Users\\fellg\\AppData\\Roaming\\Megadepth\\megadepth.exe D:/Dropbox/Code/megadepth/inst/tests/test.bam --prefix C:\\Users\\fellg\\AppData\\Local\\Temp\\RtmpKefDQ8/test.bam --threads 1 --no-head --bigwig --auc --min-unique-qual 10 --annotation  D:/Dropbox/Code/megadepth/inst/tests/test_exons.bed --frag-dist --alts --include-softclip --only-polya --read-ends --test-polya --no-annotation-stdout
+        ## Error
+        # building whole annotation region map done
+        # 2 chromosomes for annotated regions read
+        # Processing BAM: "D:/Dropbox/Code/megadepth/inst/tests/test.bam"
+        # [bwGetOverlappingIntervalsCore] Got an error
+        # [bwClose] There was an error while finishing writing a bigWig file! The output is likely truncated.
+        # [bwGetOverlappingIntervalsCore] Got an error
+        # [bwClose] There was an error while finishing writing a bigWig file! The output is likely truncated.
+        # Read 96 records
+
+    }
+
+}## Run AUC test
+total_auc <-
+    megadepth_shell(file.path(tempdir(), "test.bam.all.bw"))
 test_that("test just total auc", {
     expect_equal(
-        megadepth_shell(file.path(tempdir(), "test.bam.all.bw"))[2],
+        total_auc[length(total_auc)],
         readLines(
             "https://raw.githubusercontent.com/ChristopherWilks/megadepth/master/tests/testbw1.total_auc"
         )
     )
 })
+
+
+
+## Copy test bigwig file to the package dir
+if (FALSE) {
+    file.copy(
+        file.path(tempdir(), "test.bam.all.bw"),
+        here::here("inst", "tests", "test.bam.all.bw"),
+        overwrite = TRUE
+    )
+}
+
+
 
 ## test bigwig2mean
 megadepth_shell(
@@ -83,13 +148,17 @@ megadepth_shell(
 
 test_that("test bigwig2mean", {
     expect_equal(
-        readLines(file.path(tempdir(), "bw2.mean.annotation.tsv")),
+        readLines(file.path(
+            tempdir(), "bw2.mean.annotation.tsv"
+        )),
         readLines(
             "https://raw.githubusercontent.com/ChristopherWilks/megadepth/master/tests/testbw2.bed.mean"
         )
     )
     expect_equal(
-        read_coverage(file.path(tempdir(), "bw2.mean.annotation.tsv")),
+        read_coverage(file.path(
+            tempdir(), "bw2.mean.annotation.tsv"
+        )),
         get_coverage(
             file.path(tempdir(), "test.bam.all.bw"),
             op = "mean",
@@ -101,73 +170,77 @@ test_that("test bigwig2mean", {
 
 ## test with uniques
 megadepth_shell(
-  here::here("inst", "tests", "test3.bam"),
-  "coverage" = TRUE,
-  "min-unique-qual" = 10,
-  "bigwig" = TRUE,
-  "auc" = TRUE, 
-  "prefix" = file.path(tempdir(), "test3"),
-  "no-auc-stdout" = TRUE
+    here::here("inst", "tests", "test3.bam"),
+    "coverage" = TRUE,
+    "min-unique-qual" = 10,
+    "bigwig" = TRUE,
+    "auc" = TRUE,
+    "prefix" = file.path(tempdir(), "test3"),
+    "no-auc-stdout" = TRUE
 )
 
 test_that("test with uniques", {
-  expect_equal(
-    readLines(file.path(tempdir(), "test3.auc.tsv")),
-    readLines(
-      "https://raw.githubusercontent.com/ChristopherWilks/megadepth/master/tests/test3.auc.out.tsv"
+    expect_equal(
+        readLines(file.path(tempdir(), "test3.auc.tsv")),
+        readLines(
+            "https://raw.githubusercontent.com/ChristopherWilks/megadepth/master/tests/test3.auc.out.tsv"
+        )
     )
-  )
-  })
+})
 
 ## test long reads support for junctions
 megadepth_shell(
-  here::here("inst", "tests", "long_reads.bam"),
-  "junctions" = TRUE,
-  "prefix" = file.path(tempdir(), "long_reads.bam"),
-  "long-reads" = TRUE
+    here::here("inst", "tests", "long_reads.bam"),
+    "junctions" = TRUE,
+    "prefix" = file.path(tempdir(), "long_reads.bam"),
+    "long-reads" = TRUE
 )
 
 test_that("test long reads support for junctions", {
-  expect_equal(
-    readLines(file.path(tempdir(), "long_reads.bam.jxs.tsv")),
-    readLines(
-      "https://raw.githubusercontent.com/ChristopherWilks/megadepth/master/tests/long_reads.bam.jxs.tsv"
+    expect_equal(
+        readLines(file.path(tempdir(), "long_reads.bam.jxs.tsv")),
+        readLines(
+            "https://raw.githubusercontent.com/ChristopherWilks/megadepth/master/tests/long_reads.bam.jxs.tsv"
+        )
     )
-  )
 })
 
 # test bigwig2mean on remote BW
 megadepth_shell(
-  "http://stingray.cs.jhu.edu/data/temp/megadepth.test.bam.all.bw",
-  "op" = "mean",
-  "annotation" = pkg_file("tests", "testbw2.bed"),
-  "prefix" = file.path(tempdir(), "bw2.remote.mean"),
-  "no-annotation-stdout" = TRUE
+    "http://stingray.cs.jhu.edu/data/temp/megadepth.test.bam.all.bw",
+    "op" = "mean",
+    "annotation" = pkg_file("tests", "testbw2.bed"),
+    "prefix" = file.path(tempdir(), "bw2.remote.mean"),
+    "no-annotation-stdout" = TRUE
 )
 
 test_that("test bigwig2mean on remote bw", {
-  expect_equal(
-    readLines(file.path(tempdir(), "bw2.remote.mean.annotation.tsv")),
-    readLines(
-      "https://raw.githubusercontent.com/ChristopherWilks/megadepth/master/tests/testbw2.bed.mean"
+    expect_equal(
+        readLines(file.path(
+            tempdir(), "bw2.remote.mean.annotation.tsv"
+        )),
+        readLines(
+            "https://raw.githubusercontent.com/ChristopherWilks/megadepth/master/tests/testbw2.bed.mean"
+        )
     )
-  )
 })
 
 # only print sums use different order in BED file from what's in BW to test keep_order == true
 megadepth_shell(
-  file.path(tempdir(), "test.bam.all.bw"),
-  "--sums-only" = TRUE,
-  "annotation" = pkg_file("tests", "testbw2.bed"),
-  "prefix" = file.path(tempdir(), "test.bam.bw2"),
-  "no-annotation-stdout" = TRUE
+    file.path(tempdir(), "test.bam.all.bw"),
+    "--sums-only" = TRUE,
+    "annotation" = pkg_file("tests", "testbw2.bed"),
+    "prefix" = file.path(tempdir(), "test.bam.bw2"),
+    "no-annotation-stdout" = TRUE
 )
 
 test_that("test bigwig2mean on remote bw", {
-  expect_equal(
-    readLines(file.path(tempdir(), "test.bam.bw2.annotation.tsv")),
-    readLines(
-      "https://raw.githubusercontent.com/ChristopherWilks/megadepth/master/tests/testbw2.bed.out.tsv"
+    expect_equal(
+        readLines(file.path(
+            tempdir(), "test.bam.bw2.annotation.tsv"
+        )),
+        readLines(
+            "https://raw.githubusercontent.com/ChristopherWilks/megadepth/master/tests/testbw2.bed.out.tsv"
+        )
     )
-  )
 })
